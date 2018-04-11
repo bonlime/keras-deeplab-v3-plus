3ec39e83511f8ec2ea4d3171c83865b4ca159b68
Emil Zakirov <bonlimezak@gmail.com>
2018-04-11 15:53:19 +0300

    added keras-like documentation. added possibility to load weights when instantiating a model

--- model.py
+++ model.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 """ Deeplabv3+ model for Keras.
 This model is based on TF repo:
 https://github.com/tensorflow/models/tree/master/research/deeplab
@@ -10,35 +7,53 @@ On Pascal VOC, original model gets to 84.56% mIOU
 
 This model is only available for the TensorFlow backend,
 due to its reliance on `SeparableConvolution` layers.
+
 # Reference
-- [Encoder-Decoder with Atrous Separable Convolution 
+- [Encoder-Decoder with Atrous Separable Convolution
     for Semantic Image Segmentation](https://arxiv.org/pdf/1802.02611.pdf)
 """
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import os
 import warnings
 import numpy as np
+
 from keras.models import Model
-from keras import layers  # not sure if needed
-from keras.layers import Dense, Input, BatchNormalization, Activation, Conv2D, Concatenate, Softmax
-from keras.layers import SeparableConv2D, MaxPooling2D, DepthwiseConv2D, ZeroPadding2D
-from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, AveragePooling2D, Dropout
+from keras import layers
+from keras.layers import Input
+from keras.layers import Activation
+from keras.layers import Dense
+from keras.layers import Concatenate
+from keras.layers import Softmax
+from keras.layers import Dropout
+from keras.layers import BatchNormalization
+from keras.layers import Conv2D
+from keras.layers import SeparableConv2D
+from keras.layers import MaxPooling2D
+from keras.layers import DepthwiseConv2D
+from keras.layers import ZeroPadding2D
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import GlobalMaxPooling2D
+from keras.layers import AveragePooling2D
+from keras.engine import Layer
+from keras.engine import InputSpec
 from keras.engine.topology import get_source_inputs
-from keras.utils.data_utils import get_file
 from keras import backend as K
 from keras.applications import imagenet_utils
-from keras.applications.imagenet_utils import decode_predictions
-from keras.applications.imagenet_utils import _obtain_input_shape
-
-from keras.engine import Layer, InputSpec
 from keras.utils import conv_utils
+from keras.utils.data_utils import get_file
+
+TF_WEIGHTS_PATH = "https://github.com/bonlime/keras-deeplab-v3-plus/releases/download/1.0/deeplabv3_weights_tf_dim_ordering_tf_kernels.h5"
 
 
 class BilinearUpsampling(Layer):
     """Just a simple bilinear upsampling layer. Works only with TF.
-       Args
+       Args:
            upsampling: tuple of 2 numbers > 0. The upsampling ratio for h and w
-           output_size: used instead of upsampling arg if passed! 
+           output_size: used instead of upsampling arg if passed!
     """
 
     def __init__(self, upsampling=(2, 2), output_size=None, data_format=None, **kwargs):
@@ -88,7 +103,7 @@ class BilinearUpsampling(Layer):
 def SepConv_BN(x, filters, prefix, stride=1, kernel_size=3, rate=1, depth_activation=False, epsilon=1e-3):
     """ SepConv with BN between depthwise & pointwise. Optionally add activation after BN
         Implements right "same" padding for even kernel sizes
-        Args: 
+        Args:
             x: input tensor
             filters: num of filters in pointwise convolution
             prefix: prefix before name
@@ -98,7 +113,7 @@ def SepConv_BN(x, filters, prefix, stride=1, kernel_size=3, rate=1, depth_activa
             depth_activation: flag to use activation between depthwise & poinwise convs
             epsilon: epsilon to use in BN layer
     """
-    
+
     if stride == 1:
         depth_padding = 'same'
     else:
@@ -125,10 +140,10 @@ def SepConv_BN(x, filters, prefix, stride=1, kernel_size=3, rate=1, depth_activa
     return x
 
 
-def conv2d_same(x, filters, prefix, stride=1, kernel_size=3,  rate=1):
+def conv2d_same(x, filters, prefix, stride=1, kernel_size=3, rate=1):
     """Implements right 'same' padding for even kernel sizes
         Without this there is a 1 pixel drift when stride = 2
-        Args: 
+        Args:
             x: input tensor
             filters: num of filters in pointwise convolution
             prefix: prefix before name
@@ -161,13 +176,13 @@ def xception_block(inputs, depth_list, prefix, skip_connection_type, stride,
                    rate=1, depth_activation=False, return_skip=False):
     """ Basic building block of modified Xception network
         Args:
-            inputs: input tensor 
+            inputs: input tensor
             depth_list: number of filters in each SepConv layer. len(depth_list) == 3
             prefix: prefix before name
             skip_connection_type: one of {'conv','sum','none'}
-            stride: stride at depthwise conv
+            stride: stride at last depthwise conv
             rate: atrous rate for depthwise convolution
-            depth_activation: flag to use activation between depthwise & poinwise convs
+            depth_activation: flag to use activation between depthwise & pointwise convs
             return_skip: flag to return additional tensor after 2 SepConvs for decoder
             """
     residual = inputs
@@ -196,17 +211,46 @@ def xception_block(inputs, depth_list, prefix, skip_connection_type, stride,
         return outputs
 
 
-def Deeplabv3(input_shape, num_classes=21, last_activation=None, OS=16):
-    """Deeplab v3 model. 
-        Args:
-            input_shape: shape of input image. format HxWxC 
-            num_classes: number of desired classes. If != 21, last layer is initialized randomly
-            last_activation: flag to use sigmoid activation after models logits
-            OS: determines input_shape/feature_extractor_output ratio. One of {8,16}
+def Deeplabv3(weights='pascal_voc', input_tensor=None, input_shape=(512, 512, 3), classes=21, OS=16):
+    """ Instantiates the Deeplabv3+ architecture
+
+    Optionally loads weights pre-trained
+    on PASCAL VOC. This model is available for TensorFlow only,
+    and can only be used with inputs following the TensorFlow
+    data format `(width, height, channels)`.
+    # Arguments
+        weights: one of 'pascal_voc' (pre-trained on pascal voc)
+            or None (random initialization)
+        input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
+            to use as image input for the model.
+        input_shape: shape of input image. format HxWxC
+            PASCAL VOC model was trained on (512,512,3) images
+        classes: number of desired classes. If classes != 21,
+            last layer is initialized randomly
+        OS: determines input_shape/feature_extractor_output ratio. One of {8,16}
+
+    # Returns
+        A Keras model instance.
+
+    # Raises
+        RuntimeError: If attempting to run this model with a
+            backend that does not support separable convolutions.
+        ValueError: in case of invalid argument for `weights`
+
     """
+
+    if not (weights in {'pascal_voc', None}):
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization) or `pascal_voc` '
+                         '(pre-trained on PASCAL VOC)')
+
+    if K.backend() != 'tensorflow':
+        raise RuntimeError('The Deeplabv3+ model is only available with '
+                           'the TensorFlow backend.')
+
     if OS == 8:
         entry_block3_stride = 1
-        middle_block_rate = 2 # ! Not mentioned in paper, but required
+        middle_block_rate = 2  # ! Not mentioned in paper, but required
         exit_block_rates = (2, 4)
         atrous_rates = (12, 24, 36)
     else:
@@ -215,13 +259,20 @@ def Deeplabv3(input_shape, num_classes=21, last_activation=None, OS=16):
         exit_block_rates = (1, 2)
         atrous_rates = (6, 12, 18)
 
-    img_input = Input(input_shape)
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
     x = Conv2D(32, (3, 3), strides=(2, 2),
                name='entry_flow_conv1_1', use_bias=False, padding='same')(img_input)
     x = BatchNormalization(name='entry_flow_conv1_1_BN')(x)
     x = Activation('relu')(x)
 
-    x = conv2d_same(x, 64, 'entry_flow_conv1_2', kernel_size = 3, stride=1)
+    x = conv2d_same(x, 64, 'entry_flow_conv1_2', kernel_size=3, stride=1)
     x = BatchNormalization(name='entry_flow_conv1_2_BN')(x)
     x = Activation('relu')(x)
 
@@ -246,10 +297,9 @@ def Deeplabv3(input_shape, num_classes=21, last_activation=None, OS=16):
     x = xception_block(x, [1536, 1536, 2048], 'exit_flow_block2',
                        skip_connection_type='none', stride=1, rate=exit_block_rates[1],
                        depth_activation=True)
-
     # end of feature extractor
-    # branching for Atrous Spatial Pyramid Pooling
 
+    # branching for Atrous Spatial Pyramid Pooling
     # simple 1x1
     b0 = Conv2D(256, (1, 1), padding='same', use_bias=False, name='aspp0')(x)
     b0 = BatchNormalization(name='aspp0_BN', epsilon=1e-5)(b0)
@@ -258,10 +308,10 @@ def Deeplabv3(input_shape, num_classes=21, last_activation=None, OS=16):
     # rate = 6 (12)
     b1 = SepConv_BN(x, 256, 'aspp1',
                     rate=atrous_rates[0], depth_activation=True, epsilon=1e-5)
-    # hole = 12 (24)
+    # rate = 12 (24)
     b2 = SepConv_BN(x, 256, 'aspp2',
                     rate=atrous_rates[1], depth_activation=True, epsilon=1e-5)
-    # hole = 18 (36)
+    # rate = 18 (36)
     b3 = SepConv_BN(x, 256, 'aspp3',
                     rate=atrous_rates[2], depth_activation=True, epsilon=1e-5)
 
@@ -300,17 +350,30 @@ def Deeplabv3(input_shape, num_classes=21, last_activation=None, OS=16):
                    depth_activation=True, epsilon=1e-5)
 
     # you can use it with arbitary number of classes
-    if num_classes == 21:
+    if classes == 21:
         last_layer_name = 'logits_semantic'
     else:
         last_layer_name = 'custom_logits_semantic'
-    x = Conv2D(num_classes, (1, 1), padding='same', name=last_layer_name)(x)
-    if last_activation:
-        x = Activation('sigmoid')(x)
+
+    x = Conv2D(classes, (1, 1), padding='same', name=last_layer_name)(x)
     x = BilinearUpsampling(output_size=(input_shape[0], input_shape[1]))(x)
 
-    model = Model(img_input, x, name='deeplab')
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
 
+    model = Model(inputs, x, name='deeplabv3+')
+
+    # load weights
+
+    if weights == 'pascal_voc':
+        weights_path = get_file('deeplabv3_weights_tf_dim_ordering_tf_kernels.h5',
+                                TF_WEIGHTS_PATH,
+                                cache_subdir='models')
+        model.load_weights(weights_path, by_name=True)
     return model
 
 
@@ -319,6 +382,6 @@ def preprocess_input(x):
     # Arguments
         x: a 4D numpy array consists of RGB values within [0, 255].
     # Returns
-        Preprocessed array.
+        Input array scaled to [-1.,1.]
     """
     return imagenet_utils.preprocess_input(x, mode='tf')
