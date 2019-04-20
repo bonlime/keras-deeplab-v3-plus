9c4d3d879f6b77133ea78a66234c3f6b2bdc9e39
sachsbl <sachsbl@gmail.com>
2019-04-20 08:57:00 -0600

    making a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='keras_deeplab',
+      version='0.1',
+      description='DeepLabv3+ Model with pre-trained weights',
+      url='http://github.com/sachsbl/keras_deeplab',
+      author='Ben Sachs',
+      author_email='sachsbl@gmail.com',
+      license='MIT',
+      packages=['keras_deeplab'],
+      zip_safe=False)
